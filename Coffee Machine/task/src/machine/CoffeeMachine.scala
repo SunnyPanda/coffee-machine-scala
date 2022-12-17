c5fb2ba816c05aa29c5b17b87ef596e29f9851c5
SunnyPanda <kashtanka.nya@gmail.com>
2022-12-17 18:25:13 +0200

    Stage 3/6: Estimate the number of servings

--- Coffee Machine/task/src/machine/CoffeeMachine.scala
+++ Coffee Machine/task/src/machine/CoffeeMachine.scala
@@ -3,12 +3,23 @@ package machine
 import scala.io.StdIn.readInt
 
 object CoffeeMachine extends App {
+  println("Write how many ml of water the coffee machine has:")
+  val water = readInt()
+  println("Write how many ml of milk the coffee machine has:")
+  val milk = readInt()
+  println("Write how many grams of coffee beans the coffee machine has:")
+  val beans = readInt()
+
   println("Write how many cups of coffee you will need:")
-  val cupsOfCoffee = readInt
-  println(s"For $cupsOfCoffee cups of coffee you will need:")
-  println(
-    s"""${cupsOfCoffee * 200} ml of water
-       |${cupsOfCoffee * 50} ml of milk
-       |${cupsOfCoffee * 15} g of coffee beans
-       |""".stripMargin)
+  val cupsOfCoffeeNeeded = readInt()
+  val cupsOfCoffeeCanMake: Int = List(water / 200, milk / 50, beans / 15).min
+  val difference = cupsOfCoffeeCanMake - cupsOfCoffeeNeeded
+
+  if (difference == 0) {
+    println("Yes, I can make that amount of coffee")
+  } else if (difference > 0) {
+    println(s"Yes, I can make that amount of coffee (and even $difference more than that)")
+  } else {
+    println(s"No, I can make only $cupsOfCoffeeCanMake cup(s) of coffee")
+  }
 }
