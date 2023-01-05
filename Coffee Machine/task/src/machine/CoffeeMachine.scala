12d93f5931cf1ae9b0eb766201a5104f4aade9c4
SunnyPanda <ekaterinakozlova90@gmail.com>
2023-01-05 23:58:28 +0200

    Delete comments

--- Coffee Machine/task/src/machine/CoffeeMachine.scala
+++ Coffee Machine/task/src/machine/CoffeeMachine.scala
@@ -4,10 +4,6 @@ import scala.io.StdIn._
 import machine.CoffeeMachineProcessor.processCommand
 
 object CoffeeMachine extends App {
-
-//  def main(args: Array[String]): Unit = {
-//
-//  }
   var command = readLine("Write action (buy, fill, take, remaining, exit):\n")
   while (command != "exit") {
     processCommand(command)
